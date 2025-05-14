27ebddc0e16a296dfe1be9a15c23f7473b467334
Adrian Ruiz <arsanchez@kiconex.es>
2025-05-14 13:02:29 +0200

    Create User model & instalation bcryptjs

---
+++ back/src/models/user.js
@@ -0,0 +1,56 @@
+const bcryptjs = require("bcryptjs");
+const mysqlAdapter = require("../utils/mysqlAdapter");
+
+class User {
+  constructor() {
+    this.uuid = "";
+    this.user = "";
+    this.mail = "";
+    this.name = "";
+    this.password = "";
+  }
+
+  set(data) {
+    this.user = data.user;
+    this.mail = data.mail;
+    this.name = data.name;
+    this.password = bcryptjs.hashSync(data.password, 0);
+    return this;
+  }
+
+  getData() {
+    return {
+      uuid: this.uuid,
+      user: this.user,
+      mail: this.mail,
+      name: this.name,
+      password: this.password,
+    };
+  }
+
+  async create() {
+    const data = this.getData();
+    const result = await mysqlAdapter.query("INSERT INTO users SET ?", data);
+    return result;
+  }
+
+  async update() {
+    const data = this.getData();
+    const result = await mysqlAdapter.query("UPDATE users SET ? WHERE uuid = ?", [data, this.uuid]);
+    return result;
+  }
+
+  async delete() {
+    return mysqlAdapter.query("DELETE FROM users WHERE uuid = ?", [this.uuid]);
+  }
+
+  static userObjectList(dataList) {
+    return dataList.map(data => {
+      const user = new User(data.uuid);
+      user.set(data);
+      return user;
+    });
+  }
+}
+
+module.exports = User;
