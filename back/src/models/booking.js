e29dc24f2a0ee257015e2c9b88c9d464f5fc4bd1
dpalacios <dpalacios@kiconex.es>
2025-05-14 12:38:34 +0200

    booking model

--- back/src/models/booking.js
+++ back/src/models/booking.js
@@ -1,3 +1,5 @@
+const mysqlAdapter = require("../utils/mysqlAdapter");
+
 class Booking {
 
   contructor(uuid) {
@@ -23,5 +25,57 @@ class Booking {
     this.details = data.details;
   }
 
+  get() {
+    const data = {
+      uuid: this.uuid,
+      user_uuid: this.user_uuid,
+      room_uuid: this.room_uuid,
+      start_date: this.start_date,
+      end_date: this.end_date,
+      start_time: this.start_time,
+      end_time: this.end_time,
+      title: this.title,
+      details: this.details
+    };
+    return data;
+  }
+  
+  async create() {
+    const data = this.getData();
 
+    const result = await mysqlAdapter.query("INSERT INTO booking set ? ", data);
+    if (result.error) {
+      return result;
+    }
+  }
+
+  async update() {
+    const data = this.getData();
+    const result = await mysqlAdapter.query("UPDATE booking set ? WHERE uuid = ?", [data, this.uuid]);
+    if (result.error) {
+      return result;
+    }
+  }
+
+  async delete() {
+    const result = await mysqlAdapter.query("DELETE FROM booking WHERE uuid = ?", [this.uuid]);
+    if (result.error) {
+      return result;
+    }
+  }
+
+  async list() {
+    const rows = await mysqlAdapter.query('SELECT uuid, user_uuid, room_uuid, start_date, end_date, start_time, end_time, title, details FROM booking', []);
+    return this.bookingObjectList(rows);
+  }
+
+  bookingObjectList(dataList) {
+    return dataList.map(data => {
+      const booking = new Booking(data.uuid);
+      booking.set(data);
+      return booking;
+    });
+  }
 }
+
+module.exports = Booking;
