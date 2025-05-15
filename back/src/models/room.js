66cceea9f96977fe899a33960ea68b0378119577
dpalacios <dpalacios@kiconex.es>
2025-05-15 10:26:44 +0200

    room model updated: filter added

--- back/src/models/room.js
+++ back/src/models/room.js
@@ -48,9 +48,15 @@ class Room {
   read(filter = {}) {
     let data = [];
     let sql = "SELECT uuid, facility_uuid, name, capacity FROM rooms WHERE 1";
+
     if (Object.keys(filter).length === 0) {
       sql += " AND uuid = ?";
       data.push(this.uuid);
+    }
+
+    if (filter.facility_uuid) {
+      sql += " AND facility_uuid = ?";
+      data.push(filter.facility_uuid);
     }
 
     return mysqlAdapter.query(sql, data);
